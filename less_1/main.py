c79b169b0003a294ec39af5d7c9e506f41cc33a3
S <s@Sui-MacBookAir.local>
2022-08-31 01:01:34 +0900

    black applied

--- less_1/main.py
+++ less_1/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello_world():
-    return {"message" : "Hello World"}
+    return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 # docker run -it -p 8000:8000 {IMAGE's: id}
--- less_2/main.py
+++ less_2/main.py
@@ -4,14 +4,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello_world():
-    return {"message" : "Hello World"}
+    return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
 
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
 
 
 # docker run -it -p 8000:8000 {IMAGE's: id}
