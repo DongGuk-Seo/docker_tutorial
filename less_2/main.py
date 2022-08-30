7e1459d16e099c80633916809e562117f7fb904a
S <s@Sui-MacBookAir.local>
2022-08-31 00:04:55 +0900

    add less2

---
+++ less_2/main.py
@@ -0,0 +1,18 @@
+import os
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message" : "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
+
+
+
+# docker run -it -p 8000:8000 {IMAGE's: id}
+## -> 8000:8000 -> {myport} : {docker's port}
