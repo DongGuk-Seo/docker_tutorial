2394c294f557bbeae71ff2e1f0da75c32bd2eab8
S <s@Sui-MacBookAir.local>
2022-08-31 01:32:53 +0900

    revise main.py

--- less_2/main.py
+++ less_2/main.py
@@ -14,7 +14,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
-
-
-# docker run -it -p 8000:8000 {IMAGE's: id}
-## -> 8000:8000 -> {myport} : {docker's port}
